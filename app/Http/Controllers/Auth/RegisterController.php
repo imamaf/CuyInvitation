f367284f99c1cb8c53d24f4d4208a55bb590a18b
paypas <adearmawi@gmail.com>
2020-05-15 09:36:47 +0700

    tambahan yg useradd

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\User_attribut;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -63,10 +64,20 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        $userAttribute = User_attribut::create([
+            'user_id' => $user->id,
+            'nama' => $data['name'],
+            'jenis_kelamin' => 'belum ada pak',
+            'no_hp' => 'ini dari mana',
+            'alamat' => 'ini juga',
+            'path_foto' => 'dan ini',
+        ]);
+        return $user;
     }
 }
--- app/User_attribut.php
+++ app/User_attribut.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class User_attribut extends Model
 {
     protected $table = 'user_attribut';
+    protected $fillable = [
+        'user_id', 'nama', 'jenis_kelamin', 'no_hp', 'alamat', 'path_foto',
+    ];
     //
 }
