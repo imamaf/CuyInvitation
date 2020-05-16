da7ca86ba1bb7100f4b763a29010f242773cf57f
= <=>
2020-05-16 13:11:48 +0700

    interface update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -43,27 +43,29 @@
 		<!-- nav -->
 		<nav class="py-3 d-lg-flex">
 			<div id="logo">
-				@if(Auth::user())
-				<h1> <a href="index.html"></span> Hi {{ Auth::user()->name}}</a></h1>
-				@endif
-				<h1> <a href="index.html"><span class="fa fa-gift"></span> Cuy Invitation </a></h1>
+				<h1><a href="index.html"><span class="fa fa-gift" style="color: #FF3E75"></span> Cuy Invitation </a></h1>
 			</div>
 			<label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
 			<input type="checkbox" id="drop" />
 			<ul class="menu ml-auto mt-1">
-				<li class="active"><a href="index.html">Home</a></li>
-				<li class=""><a href="#about">About Us</a></li>
-				<li class=""><a href="#services">Services</a></li>
-				<li class=""><a href="#stats">Stats</a></li>
-				<li class=""><a href="#testi">Testimonials</a></li>
-				<li class=""><a href="#subscribe">Subscribe</a></li>
+				<li class="nav-item active"><a href="index.html">Home</a></li>
+				<li class="nav-item"><a href="#services">Services</a></li>
+				<li class="nav-item"><a href="#stats">Stats</a></li>
+				<li class="nav-item"><a href="#design">Design</a></li>
+				<li class="nav-item"><a href="#testi">Testimonials</a></li>
+				<li class="nav-item"><a href="#subscribe">Subscribe</a></li>
 				@if(Auth::user())
-				<li class=""><a href="{{ route('logout') }}"onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log out</a>
+				<li class="nav-item dropdown">
+					<a id="navbarDropdown" class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        Hi {{ Auth::user()->name}}
+					</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+						<a href="{{ route('logout') }}"onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log out</a>
+						<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+							@csrf
+						</form>
+					</div>
 				</li>
-				<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-					@csrf
-				</form>
-				
 				@endif
 			</ul>
 		</nav>
@@ -87,10 +89,9 @@
 							<li>
 								<div class="container-fluid">
 									<div class="w3ls_banner_txt">
-										<h3 class="b-w3ltxt text-capitalize mt-md-4">Matrimonial Search.</h3>
-										<h4 class="b-w3ltxt text-capitalize mt-md-2">Best Matching Couples</h4>
-										<p class="w3ls_pvt-title my-3">onec consequat sapien ut leo cursus rhoncus. Nullam dui mi, vulputate ac metus semper Nullam dui mi.
-								 Vestibulum ante. Morbi at dui nisl.</p>
+										<h3 class="b-w3ltxt text-capitalize mt-md-4">Website & Undangan</h3>
+										<h4 class="b-w3ltxt text-capitalize mt-md-2">Pernikahan Digital</h4>
+										<p class="w3ls_pvt-title my-3">Kirim e-invitation melalui WhatsApp/SMS/email otomatis serta fitur lengkap untuk mempermudah pernikahanmu.</p>
 										<a href="#about" class="btn btn-banner my-3">Read More</a>
 									</div>
 								</div>
@@ -158,7 +159,7 @@
 										@endif
 										<input id="password-confirm" placeholder="Confirm password" type="password" class="form-control" name="password_confirmation" required>
 									<button type="submit" Class="btn"> Get Started</button>
-									<span>By registering, you agree to our <a href="#">Terms & Conditions.</a></span>
+									<p>Have an account?<a href="{{ route('login') }}" style="color:#ff3e75"> Login here</a></p>
 								</div>
 							</form>
 						</div>
@@ -306,6 +307,190 @@
 	</div>
 </section>
 <!-- //stats section -->
+
+<!-- other services -->
+<section class="other_services py-5" id="why">
+	<div class="container py-lg-5 py-3">
+		<h3 class="heading mb-sm-5 mb-4">Choose Your Design </h3>
+		<div class="row">
+			<div class="col-lg-4 col-md-6">
+				<div class="grid">
+					<a href="#">
+						<img src="assets/images/couple1.jpg" alt="" class="img-fluid" />
+						<div class="info p-4">
+							<h4 class="">Design C01</h4>
+							<p class="mt-3">Rp. 200,000</p>
+							<button class="btn btn-primary">See more</button>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mt-md-0 mt-4">
+				<div class="grid">
+					<a href="#">
+						<img src="assets/images/couple2.jpg" alt="" class="img-fluid" />
+						<div class="info p-4">
+							<h4 class="">Design C02</h4>
+							<p class="mt-3">Rp. 250,000</p>
+							<button class="btn btn-primary">See more</button>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mt-lg-0 mt-4">
+				<div class="grid">
+					<a href="#">
+						<img src="assets/images/couple3.jpg" alt="" class="img-fluid" />
+						<div class="info p-4">
+							<h4 class="">Design C03</h4>
+							<p class="mt-3">Rp. 300,000</p>
+							<button class="btn btn-primary">See more</button>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //other services -->
+
+<!-- testimonials -->
+<section class="testimonials" id="testi">
+	<div class="layer py-lg-5">
+		<div class="container py-5">
+			<h3 class="heading mb-sm-5 mb-4">testimoni our customers</h3>
+			<div class="text-center">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="testi-w3pvt-grid">
+							<p>
+								<span class="fa fa-quote-left"></span> Vivamus magna justo, lacinia eget consectetur
+								sed,
+								convallis at tellus. Nulla
+								quis lorem ut libero malesuada feugiat.Nulla quis lorem ut libero malesuada
+								feugiat.
+								Donec rutrum.
+								<span class="fa ml-2 fa-quote-right"></span>
+							</p>
+						</div>
+						<div class="testi-pos">
+							<img src="assets/images/ts1.jpg" alt="" class="img-fluid rounded-circle mb-3" />
+							<h4>Tommy & Kharin</h4>
+						</div>
+					</div>
+					<div class="col-md-6 mt-md-0 mt-3">
+						<div class="testi-w3pvt-grid">
+							<p>
+								<span class="fa fa-quote-left"></span> Vivamus magna justo, lacinia eget consectetur
+								sed,
+								convallis at tellus. Nulla
+								quis lorem ut libero malesuada feugiat.Nulla quis lorem ut libero malesuada
+								feugiat.
+								Donec rutrum.
+								<span class="fa ml-2 fa-quote-right"></span>
+							</p>
+						</div>
+						<div class="testi-pos">
+							<img src="assets/images/ts2.jpg" alt="" class="img-fluid rounded-circle mb-3" />
+							<h4>Taufik & Gina</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //testimonials -->
+
+<!-- Team  -->
+<section class="team pt-5" id="couples">
+    <div class="container py-lg-5">
+		<h3 class="heading mb-sm-5 mb-4">Happy Couples</h3>
+        <div class="row">
+			<div class="team-grid col-md-3 mb-5">
+				<img src="assets/images/couple1.jpg" class="" alt="" />
+				<div class="team-info text-center">
+					<h3 class="e">Tommy & Kharin</h3>
+					<span class="">Love Couple</span>
+				</div>
+			</div>
+			<div class="team-grid col-md-3 mb-5">
+				<img src="assets/images/couple2.jpg" class="" alt="" />
+				<div class="team-info text-center">
+					<h3 class="">Ade & Yessi</h3>
+					<span class="">Love Couple</span>
+				</div>
+			</div>
+			<div class="team-grid col-md-3 mb-5">
+				<img src="assets/images/couple3.jpg" class="" alt="" />
+				<div class="team-info text-center">
+					<h3 class="">Taufik & Gina</h3>
+					<span class="">Love Couple</span>
+				</div>
+			</div>
+			<div class="team-grid col-md-3 mb-5">
+				<img src="assets/images/couple4.jpg" class="" alt="" />
+				<div class="team-info text-center">
+					<h3 class="">Gilang & Dilla</h3>
+					<span class="">Love Couple</span>
+				</div>
+			</div>
+        </div>
+    </div>
+</section>
+<!-- //Team -->
+
+<!-- subscribe -->
+<section class="subscribe" id="subscribe">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-5 d-flex subscribe-left p-lg-5 py-sm-5 py-4">
+				<div class="news-icon mr-3">
+					<span class="fa fa-paper-plane" aria-hidden="true"></span>
+				</div>
+				<div class="text">
+					<h3>Subscribe To Our Newsletter</h3>
+				</div>
+			</div>
+			<div class="col-md-7 subscribe-right p-lg-5 py-sm-5 py-4">
+				<form action="#" method="post">
+					<input type="email" name="email" placeholder="Enter your email here" required="">
+					<button class="btn1"><span class="fa fa-paper-plane" aria-hidden="true"></span></button>
+				</form>
+				<p>we never share your email with anyone else</p>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //subscribe -->
+
+<!-- footer -->
+<footer>
+	<div class="footer-layer py-sm-5 pt-5 pb-3">
+		<div class="container py-md-3">
+			<div class="footer-grid_section text-center">
+				<div class="footer-title mb-3">
+					<a href="#"><span class="fa fa-gift" style="color: #FF3E75"></span> Cuy Invitation</a>
+				</div>
+				<div class="footer-text">
+					<p>Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla quis lorem ipnut libero malesuada feugiat.
+					 Lorem ipsum dolor sit amet, consectetur elit.</p>
+				</div>
+				<ul class="social_section_1info">
+					<li class="mb-2 facebook"><a href="#"><span class="fa fa-facebook"></span></a></li>
+					<li class="mb-2 twitter"><a href="#"><span class="fa fa-twitter"></span></a></li>
+					<li class="google"><a href="#"><span class="fa fa-google-plus"></span></a></li>
+					<li class="linkedin"><a href="#"><span class="fa fa-linkedin"></span></a></li>
+					<li class="pinterest"><a href="#"><span class="fa fa-pinterest"></span></a></li>
+					<li class="vimeo"><a href="#"><span class="fa fa-vimeo"></span></a></li>
+				</ul>
+			</div>
+		</div>
+		
+	</div>
+</footer>
+<!-- //footer -->
+
 
 </body>
 </html>
